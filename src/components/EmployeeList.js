a2e296d2c2eca2847da368a61423fcbe3aa8e242
PhongNguyen299 <siphongnguyen.hcm@gmail.com>
2023-04-19 21:55:59 +0700

    V0.1

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,11 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import EmployeeItem from "./EmployeeItem";
 import './EmployeeList.css'
 
 const EmployeeList = () => {
     const EMPLOYEE_MANAGEMENT_API = "https://643f529c3dee5b763e1a29be.mockapi.io/api/";
-    const {employeeId} = useParams();
     const [employees, setEmployees] = useState([]);
 
     useEffect(() => {
@@ -18,7 +17,7 @@ const EmployeeList = () => {
             .catch(err => {
                 throw err;
             })
-    }, [employees, employeeId]);
+    }, [employees]);
 
 
     const handleCreate = () => {
